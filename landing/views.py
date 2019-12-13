7245ef01010f9d43d4eacb007d2c8d4f522ec6e7
Giovanna <Giovanna196484@hotmail.com>
2019-12-12 21:29:06 -0300

    Finish-Project

--- landing/views.py
+++ landing/views.py
@@ -3,7 +3,7 @@ from .models import Aluno, Usuario
 
 # Create your views here.
 
-def index(request):
+def home(request):
     if request.method == 'POST':
         data_usuario = Usuario()
         data_usuario.email = request.POST['email']
@@ -15,7 +15,7 @@ def index(request):
         data_aluno.frase = request.POST['frase']
         data_aluno.save()
         
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def listar(request):
     listar_frase = Aluno.objects.all(ativo=True)
@@ -23,10 +23,11 @@ def listar(request):
         'listar_frase': listar_frase
     }
     return render(request, 'lista.html', args)
-    def deletar(request):
-        item = Aluno.objects.get(id=id)
-        if itemis not none:
-            item.ativo = false 
-            item.save()
-            return redirect('aluno/listar')
+
+def deletar(request):
+    item = Aluno.objects.get(id=id)
+    if item is not none:
+        item.ativo = false 
+        item.save()
+        return redirect('aluno/listar')
         return render(request, 'lista.html')
--- sistema/urls.py
+++ sistema/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from landing import views
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('alunos/cadastro', views.index),
+    path('', views.home),
+    path('alunos/cadastro', views.home),
     path('alunos/lista', views.listar),
     path('alunos/delete/<int:id>', views.deletar)
 ]
